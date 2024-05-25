60a3031f18ea5f7ff9e32f9763fb0b72bc5ee8f6
CEOofKohler <111597664+CEOofKohler@users.noreply.github.com>
2024-05-25 01:27:31 -0230

    Update Hub.lua

--- Hub.lua
+++ Hub.lua
@@ -1,1 +1,51 @@
-
+if game.PlaceId == 183364845 then -- SPEEDRUN 4
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/CEOofKohler/Kohler-Hub/main/GAMES/SPR4"))()
+elseif game.PlaceId == 17541193 then -- PINEWOOD COMPUTER CORE
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/CEOofKohler/Kohler-Hub/main/GAMES/PCC"))()
+elseif game.PlaceId == 4799594657 then -- MURDER ISLAND 2
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/CEOofKohler/Kohler-Hub/main/GAMES/MI2"))()
+elseif game.PlaceId == 4769793533 then -- Flicker
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/CEOofKohler/Kohler-Hub/main/GAMES/Flicker"))()
+elseif game.PlaceId == 3057050847 then -- SITE 76
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/CEOofKohler/Kohler-Hub/main/GAMES/S76"))()
+elseif game.PlaceId == 8111113436 then -- TRIVA
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/CEOofKohler/Kohler-Hub/main/GAMES/Triva"))()
+elseif game.PlaceId == 2386807555 then -- UUHHH.WAV
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/CEOofKohler/Kohler-Hub/main/GAMES/UHW"))()
+elseif game.PlaceId == 10192063645 then -- THY HOOD
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/CEOofKohler/Kohler-Hub/main/GAMES/THY"))()
+elseif game.PlaceId == 740572368 then -- DECEPTION
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/CEOofKohler/Kohler-Hub/main/GAMES/Deception"))()
+elseif game.PlaceId == 947714452 then -- BAKERS VALLEY
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/CEOofKohler/Kohler-Hub/main/GAMES/BAV"))()
+elseif game.PlaceId == 46955756 then -- TOWER BATTLES
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/CEOofKohler/Kohler-Hub/main/GAMES/TB"))()
+elseif game.PlaceId == 2210085102 then -- NAVAL WARFARE
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/CEOofKohler/Kohler-Hub/main/GAMES/NAV"))()
+elseif game.PlaceId == 11217941123 then -- STUPID IDIOT CAFE
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/CEOofKohler/Kohler-Hub/main/GAMES/SIC"))()
+elseif game.PlaceId == 7044096177 then -- THE OBBY ELEVATOR
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/CEOofKohler/Kohler-Hub/main/GAMES/TOE"))()
+elseif game.PlaceId == 10782471256 then -- OBBY LEADER
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/CEOofKohler/Kohler-Hub/main/GAMES/OL"))()
+elseif game.PlaceId == 4620170611 then -- BREAK IN STORY
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/CEOofKohler/Kohler-Hub/main/GAMES/BIS"))()
+elseif game.PlaceId == 6403373529 then -- SLAP BATTLES
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/CEOofKohler/Kohler-Hub/main/GAMES/SB"))()
+elseif game.PlaceId == 8381774868 then -- SUPER SCUFFLE
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/CEOofKohler/Kohler-Hub/main/GAMES/SS"))()
+elseif game.PlaceId == 4581966615 then -- ANOMIC
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/CEOofKohler/Kohler-Hub/main/GAMES/Anomic"))()
+elseif game.PlaceId == 6808875742 then -- BE TOAST OR YOURE TOAST
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/CEOofKohler/Kohler-Hub/main/GAMES/BTOYT"))()
+elseif game.PlaceId == 7239319209 then -- OHIO
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/CEOofKohler/Kohler-Hub/main/GAMES/Ohio"))()
+elseif game.PlaceId == 10511239884 then -- READY 2 DIE
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/CEOofKohler/Kohler-Hub/main/GAMES/R2D"))()
+elseif game.PlaceId == 12068280773 then -- NAPOLEONIC WARS
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/CEOofKohler/Kohler-Hub/main/GAMES/NW"))()
+elseif game.PlaceId == 12207090333 then -- EMPIRE CLASH
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/CEOofKohler/Kohler-Hub/main/GAMES/EC"))()
+else
+    game:GetService("StarterGui"):SetCore("SendNotification", {Title = "Kohler Hub", Text = "Game not supported", Duration = 10})
+end
